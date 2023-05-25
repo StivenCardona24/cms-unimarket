940caa5b8946961507fda16ff9bf4e6cc81aae22
Stiven Cardona <stiven.cardona.0924@gmail.com>
2023-05-24 19:51:30 -0500

    new layout

---
+++ src/components/assideNav.vue
@@ -0,0 +1,65 @@
+<template>
+  
+        <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+    <el-menu-item @click="isCollapse = !isCollapse">
+      <el-icon ><Expand v-if="isCollapse" /> <Fold v-else /> </el-icon>
+    </el-menu-item>
+      <el-sub-menu index="1">
+        <template #title>
+          <el-icon><icon-menu /></el-icon>
+          <span>Productos</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1" > <nuxt-link to="/products/myProducts">Mis Productos</nuxt-link></el-menu-item>
+        </el-menu-item-group>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <template #title>Navigator Two</template>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <template #title>Navigator Three</template>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <template #title>Navigator Four</template>
+      </el-menu-item>
+     
+    </el-menu>
+   
+</template>
+
+
+<script lang="ts" setup>
+import {Expand, Fold} from "@element-plus/icons-vue"
+import { ref } from 'vue'
+import {
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from '@element-plus/icons-vue'
+
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,43 +1,33 @@
+import { AssideNav } from '../../.nuxt/components';
+
 <template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <header-nav></header-nav>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <asside-nav></asside-nav>
+        </el-aside>
+        <el-main>
+          <slot></slot>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+
+<!-- <template>
 
    <div>
     <header-nav ></header-nav>
   <div class="flex">
   <div>
 
-    <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      :collapse="isCollapse"
-      @open="handleOpen"
-      @close="handleClose"
-    >
-    <el-menu-item @click="isCollapse = !isCollapse">
-      <el-icon ><Expand v-if="isCollapse" /> <Fold v-else /> </el-icon>
-    </el-menu-item>
-      <el-sub-menu index="1">
-        <template #title>
-          <el-icon><icon-menu /></el-icon>
-          <span>Productos</span>
-        </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1" > <nuxt-link to="/products/myProducts">Mis Productos</nuxt-link></el-menu-item>
-        </el-menu-item-group>
-      </el-sub-menu>
-      <el-menu-item index="2">
-        <el-icon><icon-menu /></el-icon>
-        <template #title>Navigator Two</template>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <el-icon><document /></el-icon>
-        <template #title>Navigator Three</template>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <el-icon><setting /></el-icon>
-        <template #title>Navigator Four</template>
-      </el-menu-item>
-     
-    </el-menu>
+   <asside-nav></asside-nav>
   </div >
   <div class="m-3 w-full">
   <slot/>
@@ -47,31 +37,5 @@
 </div>
     
     
-  </template>
+  </template> -->
   
-  <script lang="ts" setup>
-  import {Expand, Fold} from "@element-plus/icons-vue"
-  import { ref } from 'vue'
-  import {
-    Document,
-    Menu as IconMenu,
-    Location,
-    Setting,
-  } from '@element-plus/icons-vue'
-
-  const isCollapse = ref(true)
-  const handleOpen = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-  }
-  const handleClose = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
-  }
-  </script>
-  
-  <style>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
-
-  </style>
